ba243b8b6e43653d210db1a0e95c90282bc45972
Fprog <fnaive@hotmail.com>
2022-12-09 12:17:15 -0300

    Changes #1
    -add more base styles (index.css)
    -add Browser Router context (main.tsx)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 const root = document.getElementById('root') as HTMLElement
 
 ReactDOM.createRoot(root).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )

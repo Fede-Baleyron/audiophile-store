f25f7d0500254db740702024850ab6ca887284a4
Federico Baleyrón <fnaive@hotmail.com>
2022-12-20 18:21:39 -0300

    fix typo

--- src/modules/shared/footer/Main.tsx
+++ src/modules/shared/footer/Main.tsx
@@ -15,7 +15,7 @@ const Main = () => {
         <p className={`txt-gray-light txt-height ${tabletBK ? '' : 'txt-center'}`}>
           Audiofilia es una tienda en la que debes parar sí o sí para cumplir con tus necesidades de audio.
           Somos un equipo pequeño amantes de la música y especialistas en sonido y, con devoción, te ayudamos
-          a conseguir lo mejor en cuanto audio. Ven y visita nuestra tienda demo - estamos abiertos los 7 días de la semana.
+          a conseguir lo mejor en cuanto a audio. Ven y visita nuestra tienda demo - estamos abiertos los 7 días de la semana.
         </p>
       </section>
       <p className='footer-rights txt-gray-light fw-bold-700'>Copyright 2022. Todos los derechos reservados.</p>

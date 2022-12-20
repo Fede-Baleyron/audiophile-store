39c9d215752ba741461aafe4b67d8dc9919b60e5
Fprog <fnaive@hotmail.com>
2022-12-20 13:48:00 -0300

    add menu item classes to anchor element

--- src/modules/shared/header/components/MenuItem.tsx
+++ src/modules/shared/header/components/MenuItem.tsx
@@ -3,8 +3,8 @@ import { MenuItemProp } from '../utils/header-types'
 export const MenuItem = (props: MenuItemProp) => {
   const classes = props?.classNames?.join(' ')
   return (
-    <li className={`menu-item ${classes || ''}`}>
-      <a href='' rel='noopener noreferrer'>{props.title}</a>
+    <li className={`menu-item`}>
+      <a className={`${classes || ''}`} href='' rel='noopener noreferrer'>{props.title}</a>
     </li>
   )
 }

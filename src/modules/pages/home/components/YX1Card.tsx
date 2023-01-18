25d2f7fb17a71d2d181fb93d9206d2b36c1b1724
Federico Baleyrón <fede.baleyron@outlook.com>
2023-01-17 22:31:38 -0300

    change yx1 classes

--- src/modules/pages/home/components/YX1Card.tsx
+++ src/modules/pages/home/components/YX1Card.tsx
@@ -3,7 +3,7 @@ import { homeImagesRoute } from '../../../helpers/imagesRoutes'
 
 export const YX1Card = () => {
   return (
-    <section className='yx1-card-wrapper'>
+    <article className='popular-card-wrapper-yx1'>
       <img 
         src={`${homeImagesRoute}/earphones-yx1-mobile.jpg`}
         alt='image auriculares yx1' 
@@ -12,6 +12,6 @@ export const YX1Card = () => {
         <h4>AURICULARES YX1</h4>
         <Button text='VER PRODUCTO' type='secondary' />
       </article>
-    </section>
+    </article>
   )
 }

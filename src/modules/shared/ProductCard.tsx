1d939c1c3646cb1c3edff28826c0fe9f5165dd90
Federico Baleyrón <fede.baleyron@outlook.com>
2023-01-30 18:24:43 -0300

    add product-card (sell view) tablet design

--- src/modules/shared/ProductCard.tsx
+++ src/modules/shared/ProductCard.tsx
@@ -8,17 +8,18 @@ import './styles/product-card.css'
 export const ProductCard = (props: ProductCardProp) => {
   const show: boolean = props.view === 'show'
   const sell: boolean = props.view === 'sell'
+  const isSellClass = sell ? 'sell' : ''
   return (
-    <article className='product-card-container'>
+    <article className={`product-card-container ${isSellClass}`}>
       <img
-        className='product-card-img'
+        className={`product-card-img ${isSellClass}`}
         src={`${productsPath}/${props.path}/image-product.jpg`}
         alt={props.name}
       />
-      <section className='product-card-info-wrapper'>
+      <section className={`product-card-info-wrapper ${isSellClass}`}>
         {props.new && <NewProductNotification importance='primary' />}
         <h5 className='product-card-name'>{props.name}</h5>
-        <p className='product-card-description'>
+        <p className={`product-card-description ${isSellClass}`}>
           {props.description}
         </p>
         {show && <Button type='primary' text='VER PRODUCTO' />}

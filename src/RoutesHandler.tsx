da263be4574ba27708aa6e6aa6b37bc13fbab996
Federico Baleyrón <fede.baleyron@outlook.com>
2023-02-01 19:37:49 -0300

    create categories routes

--- src/RoutesHandler.tsx
+++ src/RoutesHandler.tsx
@@ -1,10 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
+import { CategoryPage } from './modules/shared/CategoryPage'
 import HomeLayout from './modules/pages/home/Main'
+
+const categories: string[] = ['auriculares', 'cascos', 'parlantes']
 
 export const RoutesHandler = () => {
   return (
     <Routes>
       <Route path='/' element={<HomeLayout />} />
+      {categories.map((category, i) => (
+        <Route
+          key={i}
+          path={`/categorias/${category}`} 
+          element={<CategoryPage category={category.toUpperCase()} />} 
+        />
+      ))}
     </Routes>
   )
 }
--- src/modules/shared/header/components/MenuItem.tsx
+++ src/modules/shared/header/components/MenuItem.tsx
@@ -2,9 +2,18 @@ import { MenuItemProp } from '../utils/header-types'
 
 export const MenuItem = (props: MenuItemProp) => {
   const classes = props?.classNames?.join(' ')
+  const linkTo = props.title.toLowerCase() !== 'inicio' 
+  ? `/categorias/${props.title.toLowerCase()}`
+  : `/`
   return (
     <li className={`menu-item`}>
-      <a className={`${classes || ''}`} href='' rel='noopener noreferrer'>{props.title}</a>
+      <a 
+        className={`${classes || ''}`} 
+        href={linkTo} 
+        rel='noopener noreferrer'
+      >
+        {props.title}
+      </a>
     </li>
   )
 }

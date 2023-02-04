bdb80f7fcad237084de5f19fbadb9ddcbdd980f5
Federico Baleyrón <fede.baleyron@outlook.com>
2023-02-04 12:18:03 -0300

    add scroll to top custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RoutesHandler } from './RoutesHandler'
 import { CategoriesProvider } from './modules/pages/context/CategoriesProvider'
+import { ScrollToTop } from './ScrollToTop'
 
 function App() {
   return (
     <>
       <CategoriesProvider>
+        <ScrollToTop />
         <RoutesHandler />
       </CategoriesProvider>
     </>

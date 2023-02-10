fc71d4cbfdba0c30f1849d7a5120e41dfaaed804
Federico Baleyrón <fede.baleyron@outlook.com>
2023-02-09 22:47:23 -0300

    add fetching data when routing to item

--- src/modules/pages/dynamic-product/components/DynamicCard.tsx
+++ src/modules/pages/dynamic-product/components/DynamicCard.tsx
@@ -1,14 +1,24 @@
-import { lazy } from 'react'
+import { lazy, useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { ProductModel } from '../../../../services/servicesTypes'
 import { Loader } from '../../../shared/Loader'
+import { CategoriesContext } from '../../context/CategoriesProvider'
 import { DynamicCardProp } from '../utils/dynamic-product-types'
 import { DynamicWrapper } from './DynamicWrapper'
 const ProductCard = lazy(() => import('../../../shared/ProductCard'))
 
 export const DynamicCard = ({ items }: DynamicCardProp) => {
-  const { product } = useParams()
-  const selectedItem: ProductModel | undefined = items?.find(item => item.path === product)
+  const contextData = useContext(CategoriesContext)
+  const { product, category } = useParams()
+  let selectedItem: ProductModel | undefined = undefined
+  selectedItem = items?.find(item => item.path === product)
+  const isCategory = category !== undefined ? category : ''
+  useEffect(() => {
+    console.log(selectedItem)
+    if(contextData?.data[isCategory] === null) {
+      contextData?.getData(isCategory)
+    }
+  }, [])
   return (
     <>
       {selectedItem ?
